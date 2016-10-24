c701e95aaaad6cbc9abc6b20a5ced36648e6c91e
Gosha Krikun <gosha@krikun.org>
2016-10-25 00:31:52 +0300

    Add resource mails and by files iterator

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,29 @@
+import java.io.File
+
 object Main {
+
+  def recursiveListFiles(f: File): Array[File] = {
+    if (f.exists) {
+      val files = f.listFiles
+      files ++ files.filter(_.isDirectory).flatMap(recursiveListFiles)
+    } else {
+      Array[File]()
+    }
+  }
+
   def main(args: Array[String]):Unit = {
-    println("hey!")
+    val path = getClass.getResource("/mails")
+
+    println(path)
+    if (path.equals(null)) {
+      println("there are no mails")
+      return
+    }
+
+    val file = new File(path.toURI)
+    val files = recursiveListFiles(file)
+    println(files.length)
+    files.map(println)
+
   }
 }
